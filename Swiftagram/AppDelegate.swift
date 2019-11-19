f159bb0b3d61d14b70fde80fa1a5f6a8bb5a4f2c
Jonathan Abantao <jonabantao@gmail.com>
2019-11-18 23:33:14 -0800

    Add persistence to login

--- Swiftagram/AppDelegate.swift
+++ Swiftagram/AppDelegate.swift
@@ -13,7 +13,6 @@ import Parse
 class AppDelegate: UIResponder, UIApplicationDelegate {
 
 
-
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
         Parse.initialize(with: ParseClientConfiguration(block: { (configuration: ParseMutableClientConfiguration) -> Void in
             configuration.applicationId = "swiftagram"
